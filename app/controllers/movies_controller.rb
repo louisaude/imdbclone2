c202cda17d19e85fd4cadec9faf59fa1d19e8de5
Louis Aude <louis.aude@live.fr>
2021-01-07 15:01:27 +0100

    added movie deletion action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,12 @@ class MoviesController < ApplicationController
     end
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
